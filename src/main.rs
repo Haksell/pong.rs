33d236de2dba1d93e1e315b7e1e07e67d81f24bd
Axel Brisse <axel.brisse@gmail.com>
2024-11-12 19:18:51 +0100

    09/19

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,48 @@
 use bevy::prelude::*;
 
-fn spawn_ball(mut commands: Commands) {
-    println!("Spawning ball...");
-    commands.spawn_empty();
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    position: Position,
 }
 
-fn spawn_camera(mut commands: Commands) {
-    commands.spawn_empty().insert(Camera2dBundle::default());
+impl BallBundle {
+    fn new() -> Self {
+        Self {
+            ball: Ball,
+            position: Position(Vec2::new(0., 0.)),
+        }
+    }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_ball, spawn_camera))
+        .add_systems(Update, project_positions)
         .run();
+}
+
+fn spawn_ball(mut commands: Commands) {
+    println!("Spawning ball...");
+    commands
+        .spawn_empty()
+        .insert(Transform::default())
+        .insert(BallBundle::new());
+}
+
+fn project_positions(mut positionables: Query<(&mut Transform, &Position)>) {
+    for (mut transform, position) in &mut positionables {
+        transform.translation = position.0.extend(0.);
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty().insert(Camera2dBundle::default());
 }
